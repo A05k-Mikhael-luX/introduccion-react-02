57c9392a3b11f0239e303076473159c47e212091
A05k-Mikhael-luX <anastasiayanastasia.tarot@gmail.com>
2023-11-23 12:15:01 +0100

    Añadir commit index Ejercicio-2

---
+++ src/components/Ejercicio2/index.jsx
@@ -0,0 +1,11 @@
+import TituloEjercicio from '../TituloEjercicio'
+import Formulario from './Formulario'
+
+export default function Ejercicio2() {
+    return (
+        <section>
+            <TituloEjercicio>Este es el ejercicio2</TituloEjercicio>
+             <Formulario />
+        </section>   
+    )
+}
